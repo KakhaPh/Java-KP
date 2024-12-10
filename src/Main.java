1e2fcab0479d6fdff63cf622a796f15123f98696
European Student <student@eu.local>
2024-12-10 07:17:37 -0800

    JVM_Kakha_Putkaradze

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Created by Kakha Phutkaradze");
+        System.out.println("Created by Kakha Phutkaradze!");
     }
 }
